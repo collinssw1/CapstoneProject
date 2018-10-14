0cd89324d36283b70da30223b0817d9c9b8a04d7
Sebastian Collins <collinssw1@appstate.edu>
2018-10-14 13:29:19 -0400

    Updated Commands

--- terminal.hs
+++ terminal.hs
@@ -4,10 +4,12 @@
 -- Programming was a mistake. This entire field is nothing but trash.
 
 
-
 -- Imports
 import System.IO
-
+import System.Process
+-- import System.POSIX
+import System.Directory
+-- import System.Console.Rainbow
 
 -- Simple Main Method
 main :: IO()
@@ -29,15 +31,92 @@ userInput prompt = do
     case str of
         -- Correctly exit the program
         "exit"  -> return False
+        
         -- Change Directory
         "cd"    -> do
-            putStrLn "cd has not been implemented"
+            str2 <- setCurrentDirectory str
+            print str2
             return True
-        -- List Directory
+        
+        -- List Directory 
         "ls"    -> do
-            putStrLn "ls has not been implemented"
+            putStrLn "cd has not been implemented"
+            return True 
+        -- Clear Command
+        "clear" -> do
+            -- str2 <- clearScreen
+            _ <- System.Process.system "reset"
             return True
+        
+        -- Move Command
+        "mv"    -> do
+            putStrLn "mv has not been implemented"
+            return True
+
+        -- Manual Page (After Cild Process)
+        "man"    -> do
+            putStrLn "man has not been implemented"
+            return True
+
+        -- Make Directory
+        "mkdir"    -> do
+            putStrLn "mkdir has not been implemented"
+            return True
+
+        -- Remove Directory
+        "rmdir"    -> do
+            putStrLn "rmdir has not been implemented"
+            return True
+
+        -- Touch
+        "touch"    -> do
+            putStrLn "touch has not been implemented"
+            return True
+
+        -- Locate
+        "locate"    -> do
+            putStrLn "locate has not been implemented"
+            return True
+
+        -- Printing Working Directory
+        "pwd"   -> do
+            str2 <- getCurrentDirectory 
+            print str2
+            return True        
+   
+        -- Demo the Color Properties
+        -- "color" -> do
+                 
         -- Unknown Function
         _       -> do 
-            putStrLn ("ERROR: " ++ str ++ "is not a valid command")
+            putStrLn ("ERROR: " ++ str ++ " is not a valid command")
             userInput prompt
+
+
+-- Background Processes
+
+-- Check Child Process
+-- childProcess :: String -> IO 
+-- Taking a break as this stuff is incredibly infuriating
+-- Trying to find the Process ID is like Rocket Science
+-- Trying to kill is it performing brain surgery in space
+-- and trying to reap the dead ones is like trying to find an honest politician.
+
+
+
+
+-- Store Commands (Remembering Commands)
+-- Desc. : On the up arrow, retrieve the previous command.
+--
+-- pastCommands ::  [String]
+-- pastCommands [] = print ""
+-- pastCommands  x = print x
+-- pastCommands (x:xs) = print x + print pastCommands xs
+
+-- Executing another program
+
+
+-- Haskell Color Support
+--
+
+
